5497c7a321922431ae5fec2c95c1c76fb52d358c
ampmmn <htmnymgw@gmail.com>
2023-06-22 22:20:47 +0900

    文字のエスケープ処理

--- soyokaze/Pattern.cpp
+++ soyokaze/Pattern.cpp
@@ -7,21 +7,21 @@ CString Pattern::StripEscapeChars(const CString& pattern)
 	// 後段のwregexに値を渡したときにエスケープ記号として解釈されるのを防ぐため
 
 	CString tmp(pattern);
-	tmp.Replace(_T("\\"), _T(""));
-	tmp.Replace(_T("?"), _T(""));
-	tmp.Replace(_T("."), _T(""));
-	tmp.Replace(_T("+"), _T(""));
-	tmp.Replace(_T("^"), _T(""));
-	tmp.Replace(_T("$"), _T(""));
-	tmp.Replace(_T("["), _T(""));
-	tmp.Replace(_T("]"), _T(""));
-	tmp.Replace(_T("|"), _T(""));
-	tmp.Replace(_T("("), _T(""));
-	tmp.Replace(_T(")"), _T(""));
-	tmp.Replace(_T("{"), _T(""));
-	tmp.Replace(_T("}"), _T(""));
-	tmp.Replace(_T("!"), _T(""));
-	tmp.Replace(_T("&"), _T(""));
+	tmp.Replace(_T("\\"), _T("\\\\"));
+	tmp.Replace(_T("?"), _T("\\?"));
+	tmp.Replace(_T("."), _T("\\."));
+	tmp.Replace(_T("+"), _T("\\+"));
+	tmp.Replace(_T("^"), _T("\\^"));
+	tmp.Replace(_T("$"), _T("\\$"));
+	tmp.Replace(_T("["), _T("\\["));
+	tmp.Replace(_T("]"), _T("\\]"));
+	tmp.Replace(_T("|"), _T("\\|"));
+	tmp.Replace(_T("("), _T("\\("));
+	tmp.Replace(_T(")"), _T("\\)"));
+	tmp.Replace(_T("{"), _T("\\{"));
+	tmp.Replace(_T("}"), _T("\\}"));
+	tmp.Replace(_T("!"), _T("\\!"));
+	tmp.Replace(_T("&"), _T("\\&"));
 
 	return tmp;
 }
