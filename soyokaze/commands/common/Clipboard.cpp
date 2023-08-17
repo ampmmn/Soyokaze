5d9874a45d2b787c7d3407a382c668ade57aadd1
ampmmn <htmnymgw@gmail.com>
2023-08-17 17:19:01 +0900

    サイズ計算

--- soyokaze/commands/common/Clipboard.cpp
+++ soyokaze/commands/common/Clipboard.cpp
@@ -13,7 +13,7 @@ bool Clipboard::Copy(const CString& text)
 	// クリップボードにコピー
 	size_t bufLen = sizeof(TCHAR) * (text.GetLength() + 1);
 	GlobalAllocMemory mem(bufLen);
-	_tcscpy_s((LPTSTR)mem.Lock(), bufLen, text);
+	_tcscpy_s((LPTSTR)mem.Lock(), text.GetLength() + 1, (LPCTSTR)text);
 	mem.Unlock();
 
 	BOOL isSet=FALSE;
@@ -22,7 +22,7 @@ bool Clipboard::Copy(const CString& text)
 
 	if (isSet) {
 		// セットされた場合はクリップボードが所有権を持つため、
-		// GlobalAllocMemoryオブジェクト側の所有権を放棄する
+		// GlobalAllocMemoryオブジェクト側の所有権を放棄する(解放しない)
 		mem.Release();
 	}
 
