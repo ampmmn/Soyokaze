1821eb9ae67aec5cdb626787abf91b20cf11870b
ampmmn <htmnymgw@gmail.com>
2024-02-17 14:26:17 +0900

    Update documents.

--- doc/gendoc.bat
+++ doc/gendoc.bat
@@ -1,8 +1,6 @@
 pushd %~dp0
 
-set APPNAME=Soyokaze
-
-pandoc -c "%~dp0github-markdown.css" -s --embed-resources --standalone --toc --toc-depth=4 -f markdown -t html --metadata title="%APPNAME% ƒ}ƒjƒ…ƒAƒ‹" %~dp0help.md > %~dp0help.html
+pandoc -c "%~dp0github-markdown.css" -s --embed-resources --standalone --toc --toc-depth=4 -f markdown -t html  %~dp0help.md > %~dp0help.html
 
 popd
 
