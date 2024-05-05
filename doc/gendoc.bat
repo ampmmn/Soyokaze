4d3584e5d23e65c677e34e0f1016cea03428e35a
ampmmn <htmnymgw@gmail.com>
2024-05-05 11:14:01 +0900

    ヘルプ更新

--- doc/gendoc.bat
+++ doc/gendoc.bat
@@ -1,6 +1,20 @@
 pushd %~dp0
 
-pandoc -c "%~dp0github-markdown.css" -s --embed-resources --standalone --toc --toc-depth=4 -f markdown -t html  %~dp0help.md > %~dp0help.html
+if not exist html mkdir html
+
+call :CONVERT help
+REM call :CONVERT window-input
+REM call :CONVERT top
+REM call :CONVERT faq
 
 popd
+goto :EOF
+
+:CONVERT
+  set INPUT=%1
+	set OUTPUT=%INPUT%
+	shift
+  pandoc -c "%~dp0github-markdown.css" -s --embed-resources --standalone --toc --toc-depth=4 -f markdown -t html  %~dp0%INPUT%.md -o %OUTPUT%.html
+REM  pandoc -c "%~dp0github-markdown.css" -s --embed-resources --standalone --toc --toc-depth=4 -f markdown -t html  %~dp0%INPUT%.md --filter=pandoc-include --filter=%~dp0convertlink.py -o html\%OUTPUT%.html
+
 
