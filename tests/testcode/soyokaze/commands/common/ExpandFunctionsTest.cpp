d6b84cb6ffc6ff7506f85e9d5810190281658e77
ampmmn <htmnymgw@gmail.com>
2024-05-06 17:21:12 +0900

    CIエラー対応

--- tests/testcode/soyokaze/commands/common/ExpandFunctionsTest.cpp
+++ tests/testcode/soyokaze/commands/common/ExpandFunctionsTest.cpp
@@ -4,10 +4,11 @@
 
 using namespace launcherapp::commands::common;
 
-TEST(ExpandFunctions, ExpandEnv1)
-{
-	CString str(_T("https://www.google.com/search?q=$1"));
-	ExpandEnv(str);
-
-	EXPECT_EQ(_T("https://www.google.com/search?q=$1"), str);
-}
+// ExpandEnv関数削除によりテスト無効化
+//TEST(ExpandFunctions, ExpandEnv1)
+//{
+//	CString str(_T("https://www.google.com/search?q=$1"));
+//	ExpandEnv(str);
+//
+//	EXPECT_EQ(_T("https://www.google.com/search?q=$1"), str);
+//}
