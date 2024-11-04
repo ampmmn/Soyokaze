6ad2e85dd3ffe6199fa2f1df15b98ff40a7c2f40
ampmmn <htmnymgw@gmail.com>
2024-11-04 11:06:53 +0900

    リネーム

--- soyokaze/commands/filter/FilterCommandParam.h
+++ soyokaze/commands/filter/FilterCommandParam.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "hotkey/CommandHotkeyAttribute.h"
+#include "hotkey/CommandHotKeyAttribute.h"
 
 namespace launcherapp {
 namespace commands {
--- soyokaze/commands/simple_dict/SimpleDictParam.h
+++ soyokaze/commands/simple_dict/SimpleDictParam.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "hotkey/CommandHotkeyAttribute.h"
+#include "hotkey/CommandHotKeyAttribute.h"
 
 class CommandEntryIF;
 
