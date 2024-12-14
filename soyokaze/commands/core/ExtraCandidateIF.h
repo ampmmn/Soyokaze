d42a4545349687440260f6647d4b027e51c7be32
ampmmn <htmnymgw@gmail.com>
2024-12-14 16:47:20 +0900

    ファイル追加しわすれ

---
+++ soyokaze/commands/core/ExtraCandidateIF.h
@@ -0,0 +1,21 @@
+// あ
+#pragma once
+
+#include "commands/core/UnknownIF.h"
+
+class CommandHotKeyAttribute;
+
+namespace launcherapp {
+namespace commands {
+namespace core {
+
+class ExtraCandidate : virtual public launcherapp::core::UnknownIF
+{
+public:
+	virtual CString GetSourceName() = 0;
+};
+
+
+} // end of namespace core
+} // end of namespace commands
+} // end of namespace launcherapp
