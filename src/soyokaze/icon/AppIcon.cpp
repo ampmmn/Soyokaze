b5d8aa33d6485e8f85ead68ea117b5403336225c
ampmmn <htmnymgw@gmail.com>
2025-02-09 17:15:52 +0900

    デフォルトアイコンの保存方法を変更

--- src/soyokaze/icon/AppIcon.cpp
+++ src/soyokaze/icon/AppIcon.cpp
@@ -85,7 +85,8 @@ HICON AppIcon::IconHandle()
 		return DefaultIconHandle();
 	}
 
-	HICON h = (HICON)LoadImage(nullptr, (LPCTSTR)appIconPath, IMAGE_ICON, 32, 32, LR_LOADFROMFILE);
+	bool isShared = false;
+	HICON h = IconLoader::Get()->LoadIconFromImageFile((LPCTSTR)appIconPath, isShared);
 	if (h == nullptr) {
 		return DefaultIconHandle();
 	}
@@ -121,6 +122,8 @@ static void CopyBitmapImage(ATL::CImage& imgOut, HBITMAP bmpSrc, int bpp)
 
 bool AppIcon::Import(const CString& iconFilePath)
 {
+	SPDLOG_DEBUG("start");
+
 	// パスが無効ならエラー
 	if (PathFileExists(iconFilePath) == FALSE) {
 		return false;
@@ -128,7 +131,10 @@ bool AppIcon::Import(const CString& iconFilePath)
 
 	bool isShared = false;
 	HICON newIconHandle = IconLoader::Get()->LoadIconFromImageFile(iconFilePath, isShared);
-
+	if (newIconHandle == nullptr) {
+		spdlog::warn(_T("Failed to load icon {}"), (LPCTSTR)iconFilePath);
+		return false;
+	}
 	// アイコンハンドルを差し替え
 	if (in->mIconHandle) {
 		DestroyIcon(in->mIconHandle);
@@ -137,97 +143,8 @@ bool AppIcon::Import(const CString& iconFilePath)
 	in->mIsLoaded = true;
 
 	// 次回ロード用に保存
-#pragma pack(push, 1)
-	struct ICONDIRENTRY {
-		BYTE  bWidth;
-		BYTE  bHeight;
-		BYTE  bColorCount;
-		BYTE  bReserved;
-		WORD  wPlanes;
-		WORD  wBitCount;
-		DWORD dwBytesInRes;  // イメージ領域のサイズ
-		DWORD dwImageOffset;
-	}; 
-
-	struct ICONHEADER {
-		WORD idReserved = 0;
-		WORD idType = 1;
-		WORD idCount = 1;
-		ICONDIRENTRY entry[1];
-	} iconHdr;
-#pragma pack(pop)
-
-	ICONINFO iconInfo;
-	if (!GetIconInfo(newIconHandle, &iconInfo)) {
-		spdlog::error("Failed to get iconinfo. handle:{}", (void*)newIconHandle);
-		return false;
-	}
-
-	// アイコンからビットマップ情報を取得
-	BITMAP bmpInfo;
-	GetObject(iconInfo.hbmColor, sizeof(BITMAP), &bmpInfo);
-	BITMAP bmpInfoMask;
-	GetObject(iconInfo.hbmMask, sizeof(BITMAP), &bmpInfoMask);
-
-	int bpp = bmpInfo.bmBitsPixel <= 24 ? 24 : bmpInfo.bmBitsPixel;
-
-	// イメージデータを取得
-	ATL::CImage imgColor;
-	CopyBitmapImage(imgColor, iconInfo.hbmColor, bpp);
-	ATL::CImage imgMask;
-	CopyBitmapImage(imgMask, iconInfo.hbmMask, 1);
-
-	int nImageSizeColor = abs(imgColor.GetPitch() * imgColor.GetHeight());
-	int nImageSizeMask =  abs(imgMask.GetPitch() * imgMask.GetHeight());
-
-	// ICOファイルのヘッダーを作成
-	auto& entry = iconHdr.entry[0];
-	entry.bWidth = (BYTE)bmpInfo.bmWidth;
-	entry.bHeight = (BYTE)bmpInfo.bmHeight;
-	entry.bColorCount = 0;
-	entry.bReserved = 0;
-	entry.wPlanes = bmpInfo.bmPlanes;
-	entry.wBitCount = bpp;
-	entry.dwBytesInRes = sizeof(BITMAPINFOHEADER) + nImageSizeColor + nImageSizeMask;
-	entry.dwImageOffset = sizeof(ICONHEADER);
-
-	int width = bmpInfo.bmWidth;
-	int height = bmpInfo.bmHeight;
-
-	// ビットマップデータを保存
 	auto& filePath = in->GetAppIconPath();
-	HANDLE fileHandle = CreateFile(filePath, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
-	if (fileHandle == INVALID_HANDLE_VALUE) {
-		return false;
-	}
-
-	BITMAPINFO bmi = {};
-	ZeroMemory(&bmi, sizeof(bmi));
-	bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-	bmi.bmiHeader.biWidth = width;
-	bmi.bmiHeader.biHeight = height * 2;
-	bmi.bmiHeader.biPlanes = 1;
-	bmi.bmiHeader.biBitCount = bpp;
-
-	DWORD writtenBytes = 0;
-	WriteFile(fileHandle, &iconHdr, sizeof(iconHdr), &writtenBytes, nullptr);
-	WriteFile(fileHandle, &bmi.bmiHeader, sizeof(BITMAPINFOHEADER), &writtenBytes, nullptr);
-	void* ptr = nullptr;
-
-	ptr = imgColor.GetPitch() < 0 ? ((char*)imgColor.GetBits()) + imgColor.GetPitch() * (imgColor.GetHeight()-1)
-		                          : ((char*)imgColor.GetBits());
-	WriteFile(fileHandle, ptr, nImageSizeColor, &writtenBytes, nullptr);
-	
-	ptr = imgMask.GetPitch() < 0 ? ((char*)imgMask.GetBits()) + imgMask.GetPitch() * (imgMask.GetHeight() - 1)
-	                             : ((char*)imgMask.GetBits());
-
-	WriteFile(fileHandle, ptr, nImageSizeMask, &writtenBytes, nullptr);
-
-	CloseHandle(fileHandle);
-
-	// リソースを解放
-	DeleteObject(iconInfo.hbmColor);
-	DeleteObject(iconInfo.hbmMask);
+	CopyFile(iconFilePath, filePath, FALSE);
 
 	SPDLOG_DEBUG("end");
 
