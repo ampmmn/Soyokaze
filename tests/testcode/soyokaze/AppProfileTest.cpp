5783c6a610349f6dfaf48d173015084dbc119502
ampmmn <htmnymgw@gmail.com>
2025-03-03 17:29:08 +0900

    テストがビルドできないのを修正

--- tests/testcode/soyokaze/AppProfileTest.cpp
+++ tests/testcode/soyokaze/AppProfileTest.cpp
@@ -6,17 +6,28 @@
 TEST(AppProfile, GetDirPath)
 {
 	TCHAR path[1024];
-	CAppProfile::GetDirPath(path, 1024);
+	CAppProfile::GetDirPath(path, 1024, false);
 
 	std::wregex re(LR"(^.:\\Users\\.+?\\\.soyokaze)");
 
 	EXPECT_TRUE(std::regex_match(path, re));
 }
 
+TEST(AppProfile, GetDirPath2)
+{
+	TCHAR path[1024];
+	CAppProfile::GetDirPath(path, 1024, true);
+
+	std::wregex re(LR"(^.:\\Users\\.+?\\\.soyokaze\\per_machine\\.+?)");
+
+	EXPECT_TRUE(std::regex_match(path, re));
+}
+
+
 TEST(AppProfile, GetFilePath)
 {
 	TCHAR path[1024];
-	CAppProfile::GetFilePath(path, 1024);
+	CAppProfile::GetFilePath(path, 1024, false);
 
 	std::wregex re(LR"(^.:\\Users\\.+?\\\.soyokaze\\settings.ini)");
 
