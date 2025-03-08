2fdc3f4d60b4c5e7eb14b171011152db8ac2b951
ampmmn <htmnymgw@gmail.com>
2025-03-08 16:06:46 +0900

    アプリ名を上書きしやすくするための仕組み(特定環境向けのカスタマイズバージョンを作成する都合)

--- src/soyokaze/app/AppName.h
+++ src/soyokaze/app/AppName.h
@@ -1,11 +1,16 @@
 #pragma once
 
 
+#define APPNAME_ "Soyokaze"
 #define APPNAME _T("Soyokaze")
 #define APPNAME_LOWERCASE _T("soyokaze")
+#define APP_EXENAME_ "soyokaze.exe"
 #define APP_PROFILE_DIRNAME _T(".soyokaze")
 #define APPLOGNAME APPNAME_LOWERCASE _T(".log")
+#define COPYRIGHT_STR "ymgw.  All rights reserved."
 #define LAUNCHER_APPID _T("{A04DA194-3C5E-454C-9C74-E9E740F85A4F}")
+
+#include "app/override.h"
 
 // {B8D984A3-84C3-43E7-92D6-B09298F88A42}
 static constexpr GUID LAUNCHER_TOAST_CALLBACK_GUID = 
