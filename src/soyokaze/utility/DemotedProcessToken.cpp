1a2f6a6c3ddc86cb1662bc87574ff7cbe03da0aa
ampmmn <htmnymgw@gmail.com>
2025-03-21 10:43:17 +0900

    コメント追加

--- src/soyokaze/utility/DemotedProcessToken.cpp
+++ src/soyokaze/utility/DemotedProcessToken.cpp
@@ -5,6 +5,9 @@
 #define new DEBUG_NEW
 #endif
 
+/**
+ * @brief デフォルトコンストラクタ
+ */
 DemotedProcessToken::DemotedProcessToken() : 
 	mShProcTokenHandle(nullptr),
 	mPrimaryToken(nullptr),
@@ -12,13 +15,21 @@ DemotedProcessToken::DemotedProcessToken() :
 {
 }
 
+/**
+ * @brief デストラクタ
+ */
 DemotedProcessToken::~DemotedProcessToken()
 {
+	// ハンドルを閉じる
 	CloseHandle(mShProcTokenHandle);
 	CloseHandle(mPrimaryToken);
 	CloseHandle(mShellProcessHandle);
 }
 
+/**
+ * @brief プライマリトークンを取得する
+ * @return プライマリトークンのハンドル、失敗した場合は nullptr
+ */
 HANDLE DemotedProcessToken::FetchPrimaryToken()
 {
 	if (mPrimaryToken) {
@@ -32,6 +43,7 @@ HANDLE DemotedProcessToken::FetchPrimaryToken()
 		return nullptr;
 	}
 
+	// トークンの特権を調整
 	TOKEN_PRIVILEGES tkp;
 	tkp.PrivilegeCount = 1;
 	LookupPrivilegeValueW(nullptr, SE_INCREASE_QUOTA_NAME, &tkp.Privileges[0].Luid);
@@ -46,6 +58,7 @@ HANDLE DemotedProcessToken::FetchPrimaryToken()
 		return nullptr;
 	}
 
+	// デスクトップシェルウィンドウのハンドルを取得
 	HWND hwnd = GetShellWindow();
 	if (hwnd == nullptr) {
 		spdlog::error("No desktop shell is present");
@@ -55,25 +68,27 @@ HANDLE DemotedProcessToken::FetchPrimaryToken()
 	// デスクトップシェルプロセスのプロセスIDを取得
 	DWORD dwPID = 0;
 	GetWindowThreadProcessId(hwnd, &dwPID);
-	if (dwPID == 0)	{
+	if (dwPID == 0) {
 		spdlog::error("Unable to get PID of desktop shell.");
 		return nullptr;
 	}
 
+	// デスクトップシェルプロセスのハンドルを開く
 	mShellProcessHandle = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, dwPID);
 	if (mShellProcessHandle == nullptr) {
 		spdlog::error("Can't open desktop shell process:  {}", GetLastError());
 		return nullptr;
 	}
 
+	// デスクトップシェルプロセスのトークンを取得
 	BOOL ret = OpenProcessToken(mShellProcessHandle, TOKEN_DUPLICATE, &mShProcTokenHandle);
 	if (ret == FALSE) {
 		spdlog::error("Can't get process token of desktop shell: {}", GetLastError());
 		return nullptr;
 	}
 
+	// トークンを複製してプライマリトークンを作成
 	const DWORD dwTokenRights = TOKEN_QUERY | TOKEN_ASSIGN_PRIMARY | TOKEN_DUPLICATE | TOKEN_ADJUST_DEFAULT | TOKEN_ADJUST_SESSIONID;
-
 	ret = DuplicateTokenEx(mShProcTokenHandle, dwTokenRights, NULL, SecurityImpersonation, TokenPrimary, &mPrimaryToken);
 	if (ret == FALSE) {
 		spdlog::error("Can't get primary token: {} ", GetLastError());
--- src/soyokaze/utility/RegistryKey.cpp
+++ src/soyokaze/utility/RegistryKey.cpp
@@ -5,20 +5,31 @@
 #define new DEBUG_NEW
 #endif
 
+/**
+ * @brief デフォルトコンストラクタ
+ */
 RegistryKey::RegistryKey() : mKey(nullptr)
 {
 }
 
+/**
+ * @brief HKEY を受け取るコンストラクタ
+ * @param hKey レジストリキーのハンドル
+ */
 RegistryKey::RegistryKey(HKEY hKey) : mKey(hKey)
 {
 }
 
+/**
+ * @brief デストラクタ
+ */
 RegistryKey::~RegistryKey()
 {
 	if (mKey == nullptr) {
-		return ;
+		return;
 	}
 
+	// HKEY_LOCAL_MACHINE, HKEY_CLASSES_ROOT, HKEY_CURRENT_USER 以外のキーを閉じる
 	bool isHKLM = mKey == HKEY_LOCAL_MACHINE;
 	bool isHKCR = mKey == HKEY_CLASSES_ROOT;
 	bool isHKCU = mKey == HKEY_CURRENT_USER;
@@ -29,6 +40,12 @@ RegistryKey::~RegistryKey()
 	}
 }
 
+/**
+ * @brief サブキーの名前を列挙する
+ * @param subKey サブキーの名前
+ * @param subKeyNames サブキーの名前を格納するベクター
+ * @return 列挙に成功した場合は true、失敗した場合は false
+ */
 bool RegistryKey::EnumSubKeyNames(LPCTSTR subKey, std::vector<CString>& subKeyNames)
 {
 	if (mKey == nullptr) {
@@ -42,6 +59,11 @@ bool RegistryKey::EnumSubKeyNames(LPCTSTR subKey, std::vector<CString>& subKeyNa
 	return subKeyObj.EnumSubKeyNames(subKeyNames);
 }
 
+/**
+ * @brief サブキーの名前を列挙する（オーバーロード）
+ * @param subKeyNames サブキーの名前を格納するベクター
+ * @return 列挙に成功した場合は true、失敗した場合は false
+ */
 bool RegistryKey::EnumSubKeyNames(std::vector<CString>& subKeyNames)
 {
 	if (mKey == nullptr) {
@@ -51,6 +73,7 @@ bool RegistryKey::EnumSubKeyNames(std::vector<CString>& subKeyNames)
 	for (int index = 0; ; ++index) {
 		TCHAR buf[1024] = { _T('\0') };
 		DWORD len = 1024;
+		// サブキーの名前を列挙
 		if (RegEnumKey(mKey, index, buf, len) != ERROR_SUCCESS) {
 			break;
 		}
@@ -61,6 +84,12 @@ bool RegistryKey::EnumSubKeyNames(std::vector<CString>& subKeyNames)
 	return true;
 }
 
+/**
+ * @brief 値の名前を列挙する
+ * @param subKey サブキーの名前
+ * @param valueNames 値の名前を格納するベクター
+ * @return 列挙に成功した場合は true、失敗した場合は false
+ */
 bool RegistryKey::EnumValueNames(LPCTSTR subKey, std::vector<CString>& valueNames)
 {
 	if (mKey == nullptr) {
@@ -74,6 +103,11 @@ bool RegistryKey::EnumValueNames(LPCTSTR subKey, std::vector<CString>& valueName
 	return subKeyObj.EnumValueNames(valueNames);
 }
 
+/**
+ * @brief 値の名前を列挙する（オーバーロード）
+ * @param valueNames 値の名前を格納するベクター
+ * @return 列挙に成功した場合は true、失敗した場合は false
+ */
 bool RegistryKey::EnumValueNames(std::vector<CString>& valueNames)
 {
 	if (mKey == nullptr) {
@@ -84,6 +118,7 @@ bool RegistryKey::EnumValueNames(std::vector<CString>& valueNames)
 	for (int index = 0; ; ++index) {
 		TCHAR buf[1024] = { _T('\0') };
 		DWORD len = 1024;
+		// 値の名前を列挙
 		if (RegEnumValue(mKey, index, buf, &len, NULL, NULL, NULL, NULL) != ERROR_SUCCESS) {
 			break;
 		}
@@ -94,6 +129,12 @@ bool RegistryKey::EnumValueNames(std::vector<CString>& valueNames)
 	return true;
 }
 
+/**
+ * @brief サブキーを開く
+ * @param subKeyName サブキーの名前
+ * @param subKey 開いたサブキーを格納する RegistryKey オブジェクト
+ * @return 開くのに成功した場合は true、失敗した場合は false
+ */
 bool RegistryKey::OpenSubKey(LPCTSTR subKeyName, RegistryKey& subKey)
 {
 	if (mKey == nullptr) {
@@ -109,14 +150,22 @@ bool RegistryKey::OpenSubKey(LPCTSTR subKeyName, RegistryKey& subKey)
 		RegCloseKey(subKey.mKey);
 	}
 	subKey.mKey = hkey;
-	
+
 	return true;
 }
 
+/**
+ * @brief レジストリ値を取得する
+ * @param subKey サブキーの名前
+ * @param valueName 値の名前
+ * @param value 取得した値を格納する CString オブジェクト
+ * @return 取得に成功した場合は true、失敗した場合は false
+ */
 bool RegistryKey::GetValue(LPCTSTR subKey, LPCTSTR valueName, CString& value)
 {
 	std::vector<TCHAR> buff(MAX_PATH_NTFS);
 	DWORD len = MAX_PATH_NTFS;
+	// レジストリ値を取得
 	if (RegGetValue(mKey, subKey, valueName, RRF_RT_REG_SZ | RRF_RT_REG_EXPAND_SZ, nullptr, &buff.front(), &len) != ERROR_SUCCESS) {
 		return false;
 	}
@@ -125,8 +174,13 @@ bool RegistryKey::GetValue(LPCTSTR subKey, LPCTSTR valueName, CString& value)
 	return true;
 }
 
+/**
+ * @brief レジストリ値を取得する（オーバーロード）
+ * @param valueName 値の名前
+ * @param value 取得した値を格納する CString オブジェクト
+ * @return 取得に成功した場合は true、失敗した場合は false
+ */
 bool RegistryKey::GetValue(LPCTSTR valueName, CString& value)
 {
 	return GetValue(nullptr, valueName, value);
 }
-
