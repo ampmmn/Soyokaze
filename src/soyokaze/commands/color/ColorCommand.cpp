227986537bc5bc600829477cf81d326e36629dcf
ampmmn <htmnymgw@gmail.com>
2025-05-31 14:12:17 +0900

    #100 カラーコマンドのアイコンがでかい

--- src/soyokaze/commands/color/ColorCommand.cpp
+++ src/soyokaze/commands/color/ColorCommand.cpp
@@ -18,12 +18,85 @@ namespace launcherapp {
 namespace commands {
 namespace color {
 
+// カラーアイコンのサイズ
+constexpr int ICON_SIZE = 64;
+constexpr int ICON_MARGIN = 8;
+constexpr int ROUND_SIZE = 8;
+
+
 struct ColorCommand::PImpl
 {
+	void CreateColorIcon(COLORREF cr);
+
 	HICON mColorIcon{nullptr};
 	COLORREF mColor{RGB(0, 0, 0)};
 	int mFormatType{TYPE_HEX6};
 };
+
+void ColorCommand::PImpl::CreateColorIcon(COLORREF cr)
+{
+	// カラービットマップ作成
+	ATL::CImage colorBmp;
+	colorBmp.Create(ICON_SIZE, ICON_SIZE, 24);
+
+	// カラービットマップの描画
+	HDC bmpDC = colorBmp.GetDC();
+
+	HBRUSH br = CreateSolidBrush(cr);
+	HGDIOBJ hOrgBr = SelectObject(bmpDC, br);
+
+	HPEN pen = (HPEN)CreatePen(PS_SOLID, 1, cr);
+	auto hOrgPen = SelectObject(bmpDC, pen);
+
+	PatBlt(bmpDC, 0, 0, ICON_SIZE, ICON_SIZE, BLACKNESS);
+	RoundRect(bmpDC, ICON_MARGIN, ICON_MARGIN, ICON_SIZE-ICON_MARGIN, ICON_SIZE-ICON_MARGIN, ROUND_SIZE, ROUND_SIZE);
+	SelectObject(bmpDC, hOrgPen);
+	DeleteObject(pen);
+	SelectObject(bmpDC, hOrgBr);
+	DeleteObject(br);
+
+	colorBmp.ReleaseDC();
+
+	// マスクビットマップ作成
+	SharedHwnd sharedWnd;
+	HDC hdc = GetDC(sharedWnd.GetHwnd());
+	HBITMAP maskBmp = CreateCompatibleBitmap(hdc, ICON_SIZE, ICON_SIZE);
+	HDC memDC = CreateCompatibleDC(hdc);
+	HGDIOBJ hOrgBmp = SelectObject(memDC, maskBmp);
+
+	struct BGR {
+		BYTE b;
+		BYTE g;
+		BYTE r;
+	};
+
+	LPBYTE head = (LPBYTE)colorBmp.GetBits();
+	int w = colorBmp.GetWidth();
+	int h = colorBmp.GetHeight();
+	for (int y = 0; y < h; ++y) {
+		BGR* p = (BGR*)(head + colorBmp.GetPitch() * y);
+		for (int x = 0; x < w; ++x) {
+			bool isBGPixel = RGB(p[x].r, p[x].g, p[x].b) != cr;
+			SetPixel(memDC, x, y, isBGPixel ? RGB(255, 255, 255) : RGB(0, 0, 0));
+		}
+	}
+
+	SelectObject(memDC, hOrgBmp);
+	DeleteDC(memDC);
+
+	ReleaseDC(sharedWnd.GetHwnd(), hdc);
+
+	// アイコン作成
+	ICONINFO iconInfo;
+	iconInfo.fIcon = TRUE;
+	iconInfo.hbmMask = maskBmp;
+	iconInfo.hbmColor = (HBITMAP)colorBmp;
+
+	mColorIcon = CreateIconIndirect(&iconInfo);
+
+	DeleteObject(maskBmp);
+}
+
 
 IMPLEMENT_ADHOCCOMMAND_UNKNOWNIF(ColorCommand)
 
@@ -65,39 +138,7 @@ ColorCommand::ColorCommand(COLORREF cr, int formatType) : in(std::make_unique<PI
 	in->mFormatType = formatType;
 
 	// カラーアイコンの作成
-	SharedHwnd sharedWnd;
-	HDC hdc = GetDC(sharedWnd.GetHwnd());
-
-	HBITMAP maskBmp = CreateCompatibleBitmap(hdc, 0, 0);
-	HBITMAP colorBmp = CreateCompatibleBitmap(hdc, 1, 1);
-
-	HDC memDC = CreateCompatibleDC(hdc);
-	HGDIOBJ hOrgBmp = SelectObject(memDC, maskBmp);
-	PatBlt(memDC, 0, 0, 1, 1, BLACKNESS);
-
-	HBRUSH br = CreateSolidBrush(cr);
-
-	SelectObject(memDC, colorBmp);
-	HGDIOBJ hOrgBr = SelectObject(memDC, br);
-	PatBlt(memDC, 0, 0, 1, 1, PATCOPY);
-
-	SelectObject(memDC, hOrgBmp);
-	SelectObject(memDC, hOrgBr);
-
-	DeleteObject(br);
-	DeleteDC(memDC);
-	ReleaseDC(sharedWnd.GetHwnd(), hdc);
-
-	ICONINFO iconInfo;
-	iconInfo.fIcon = TRUE;
-	iconInfo.hbmMask = maskBmp;
-	iconInfo.hbmColor = colorBmp;
-
-	// 
-	in->mColorIcon = CreateIconIndirect(&iconInfo);
-
-	DeleteObject(maskBmp);
-	DeleteObject(colorBmp);
+	in->CreateColorIcon(cr);
 }
 
 ColorCommand::~ColorCommand()
