d2e39337d324d43ed784dbeda60eb9812c0d333b
ampmmn <htmnymgw@gmail.com>
2025-09-08 23:12:49 +0900

    feat : #167 Web履歴検索機能の一部の設定値が保存できない

--- src/soyokaze/commands/webhistory/WebHistoryCommandParam.cpp
+++ src/soyokaze/commands/webhistory/WebHistoryCommandParam.cpp
@@ -5,15 +5,15 @@ namespace launcherapp { namespace commands { namespace webhistory {
 
 bool CommandParam::Save(Settings& settings) const
 {
-	// Note: AppPreference$B$K$b(BGetter$B$,$"$k$N$G%-!<$r9g$o$;$k$3$H(B!
+	// Note: AppPreferenceにもGetterがあるのでキーを合わせること!
 	settings.Set(_T("Browser:EnableWebHistory"), mIsEnable);
 	settings.Set(_T("Browser:PrefixWebHistory"), mPrefix);
-	settings.Set(_T("Browser::Candidates"), mLimit);
+	settings.Set(_T("Browser:Candidates"), mLimit);
 	settings.Set(_T("Browser:WebHistoryMinTriggerLength"), mMinTriggerLength);
-	settings.Set(_T("Browser::EnableHistoryEdge"), mIsEnableEdge);
-	settings.Set(_T("Browser::EnableHistoryChrome"), mIsEnableChrome);
-	settings.Set(_T("Browser::UseMigemo"), mIsUseMigemo);
-	settings.Set(_T("Browser::UseURL"), mIsUseURL);
+	settings.Set(_T("Browser:EnableHistoryEdge"), mIsEnableEdge);
+	settings.Set(_T("Browser:EnableHistoryChrome"), mIsEnableChrome);
+	settings.Set(_T("Browser:UseMigemo"), mIsUseMigemo);
+	settings.Set(_T("Browser:UseURL"), mIsUseURL);
 
 	return true;
 }
@@ -22,12 +22,12 @@ bool CommandParam::Load(Settings& settings)
 {
 	mIsEnable = settings.Get(_T("Browser:EnableWebHistory"), true);
 	mPrefix = settings.Get(_T("Browser:PrefixWebHistory"), _T(""));
-	mLimit = settings.Get(_T("Browser::Candidates"), 20);
-	mMinTriggerLength = settings.Get(_T("WebHistoryMinTriggerLength"), 5);
-	mIsEnableEdge = settings.Get(_T("Browser::EnableHistoryEdge"), true);
-	mIsEnableChrome = settings.Get(_T("Browser::EnableHistoryChrome"), true);
-	mIsUseMigemo = settings.Get(_T("Browser::UseMigemo"), true);
-	mIsUseURL = settings.Get(_T("Browser::UseURL"), false);
+	mLimit = settings.Get(_T("Browser:Candidates"), 20);
+	mMinTriggerLength = settings.Get(_T("Browser:WebHistoryMinTriggerLength"), 5);
+	mIsEnableEdge = settings.Get(_T("Browser:EnableHistoryEdge"), true);
+	mIsEnableChrome = settings.Get(_T("Browser:EnableHistoryChrome"), true);
+	mIsUseMigemo = settings.Get(_T("Browser:UseMigemo"), true);
+	mIsUseURL = settings.Get(_T("Browser:UseURL"), false);
 
 	return true;
 }
