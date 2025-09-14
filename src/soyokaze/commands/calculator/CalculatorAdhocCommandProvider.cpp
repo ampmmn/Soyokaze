f9d8462d42db3a3f97cbd008d83c1f7e5dca2036
ampmmn <htmnymgw@gmail.com>
2025-09-14 16:40:17 +0900

    feat : #170 【電卓機能】式の先頭の項の基数に合わせて計算結果の順序を変える

--- src/soyokaze/commands/calculator/CalculatorAdhocCommandProvider.cpp
+++ src/soyokaze/commands/calculator/CalculatorAdhocCommandProvider.cpp
@@ -43,8 +43,8 @@ struct CalculatorAdhocCommandProvider::PImpl : public AppPreferenceListenerIF
 	//
 	Calculator mCalc;
 
-	// 環境変数PATHにあるexeを実行するためのコマンド
-	CalculatorCommand* mCommandPtr{nullptr};
+	// 10進数で結果表示用のコマンド
+	CalculatorCommand* mDecResultPtr{nullptr};
 	// 16進数で結果表示用のコマンド
 	CalculatorCommand* mHexResultPtr{nullptr};
 	// 8進数で結果表示用のコマンド
@@ -64,7 +64,7 @@ REGISTER_COMMANDPROVIDER(CalculatorAdhocCommandProvider)
 CalculatorAdhocCommandProvider::CalculatorAdhocCommandProvider() : in(std::make_unique<PImpl>())
 {
 	in->mIsEnable = false;
-	in->mCommandPtr = new CalculatorCommand();
+	in->mDecResultPtr = new CalculatorCommand();
 	in->mHexResultPtr = new CalculatorCommand(16);
 	in->mOctResultPtr = new CalculatorCommand(8);
 	in->mBinResultPtr = new CalculatorCommand(2);
@@ -81,8 +81,8 @@ CalculatorAdhocCommandProvider::~CalculatorAdhocCommandProvider()
 	if (in->mHexResultPtr) {
 		in->mHexResultPtr->Release();
 	}
-	if (in->mCommandPtr) {
-		in->mCommandPtr->Release();
+	if (in->mDecResultPtr) {
+		in->mDecResultPtr->Release();
 	}
 }
 
@@ -122,43 +122,88 @@ void CalculatorAdhocCommandProvider::QueryAdhocCommands(
 	// 例: 1 -> 1 のようなケース
 	bool isBuiltinFunction = result.GetLength() > 0 && result[0] == _T('<');
 	if (isBuiltinFunction == false && cmdline != result) {
-		in->mCommandPtr->SetResult(result);
-		in->mCommandPtr->AddRef();
-		commands.Add(CommandQueryItem(Pattern::PartialMatch, in->mCommandPtr));
+		in->mDecResultPtr->SetResult(result);
 	}
 
-	// もし、評価結果が整数値なら16/8/2進数の結果も表示する
 	static tregex regexInt(_T("^-?[0-9]+$"));
-	if (std::regex_match(tstring(result), regexInt)) {
-		CString numStr(result);
+	if (std::regex_match(tstring(result), regexInt) == false) {
+		// 評価結果が整数値でない場合は、10進数の結果のみを表示
+		in->mDecResultPtr->AddRef();
+		commands.Add(CommandQueryItem(Pattern::FrontMatch, in->mDecResultPtr));
+		return ;
+	}
+	// もし、評価結果が整数値なら16/8/2進数の結果も表示する
 
-		CString fmtStr;
+	// 先頭の要素が10進数/16進数/8進数/2進数かで先に表示する進数を替える
+	static const int defaultOrder[] = { 10, 16, 8, 2};
 
-		// 16進数
-		fmtStr.Format(_T("hex(%s)"), (LPCTSTR)numStr);
-		if (in->mCalc.Evaluate(fmtStr, result)) {
-			result.Replace(_T("'"), _T(""));
-			in->mHexResultPtr->SetResult(result);
-			in->mHexResultPtr->AddRef();
-			commands.Add(CommandQueryItem(Pattern::PartialMatch, in->mHexResultPtr));
-		}
+	const int* order = defaultOrder;
 
-		// 8
-		fmtStr.Format(_T("oct(%s)"), (LPCTSTR)numStr);
-		if (in->mCalc.Evaluate(fmtStr, result)) {
-			result.Replace(_T("'"), _T(""));
-			in->mOctResultPtr->SetResult(result);
-			in->mOctResultPtr->AddRef();
-			commands.Add(CommandQueryItem(Pattern::PartialMatch, in->mOctResultPtr));
-		}
+	std::wstring cmdline_(cmdline);
+	static tregex regHex(_T("^ *0x"));
+	static tregex regOct(_T("^ *0o"));
+	static tregex regBin(_T("^ *0b"));
 
-		// 2進数
-		fmtStr.Format(_T("bin(%s)"), (LPCTSTR)numStr);
-		if (in->mCalc.Evaluate(fmtStr, result)) {
-			result.Replace(_T("'"), _T(""));
-			in->mBinResultPtr->SetResult(result);
+	if (std::regex_search(cmdline_, regHex)) {
+		// 16進数を先に表示
+		static const int hexFirstOrder[] = { 16, 10, 8, 2};
+		order = hexFirstOrder;
+	}
+	else if (std::regex_search(cmdline_, regOct) != false) {
+		// 8進数を先に表示
+		static const int octFirstOrder[] = { 8, 10, 16, 2};
+		order = octFirstOrder;
+	}
+	else if (std::regex_search(cmdline_, regBin) != false) {
+		// 2進数を先に表示
+		static const int binFirstOrder[] = { 2, 10, 16, 8};
+		order = binFirstOrder;
+	}
+
+	CString numStr(result);
+
+	CString fmtStr;
+
+	// 16進数変換
+	fmtStr.Format(_T("hex(%s)"), (LPCTSTR)numStr);
+	if (in->mCalc.Evaluate(fmtStr, result)) {
+		result.Replace(_T("'"), _T(""));
+		in->mHexResultPtr->SetResult(result);
+	}
+
+	// 8進数変換
+	fmtStr.Format(_T("oct(%s)"), (LPCTSTR)numStr);
+	if (in->mCalc.Evaluate(fmtStr, result)) {
+		result.Replace(_T("'"), _T(""));
+		in->mOctResultPtr->SetResult(result);
+	}
+
+	// 2進数変換
+	fmtStr.Format(_T("bin(%s)"), (LPCTSTR)numStr);
+	if (in->mCalc.Evaluate(fmtStr, result)) {
+		result.Replace(_T("'"), _T(""));
+		in->mBinResultPtr->SetResult(result);
+	}
+
+	// orderの順序に従って結果を格納する
+	for (int i = 0; i < 4; ++i) {
+		switch(order[i]) {
+		case 2:
 			in->mBinResultPtr->AddRef();
-			commands.Add(CommandQueryItem(Pattern::PartialMatch, in->mBinResultPtr));
+			commands.Add(CommandQueryItem(Pattern::FrontMatch, in->mBinResultPtr));
+			break;
+		case 8:
+			in->mOctResultPtr->AddRef();
+			commands.Add(CommandQueryItem(Pattern::FrontMatch, in->mOctResultPtr));
+			break;
+		case 10:
+			in->mDecResultPtr->AddRef();
+			commands.Add(CommandQueryItem(Pattern::FrontMatch, in->mDecResultPtr));
+			break;
+		case 16:
+			in->mHexResultPtr->AddRef();
+			commands.Add(CommandQueryItem(Pattern::FrontMatch, in->mHexResultPtr));
+			break;
 		}
 	}
 }
