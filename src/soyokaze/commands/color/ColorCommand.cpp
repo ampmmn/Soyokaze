b193703359d0c08e33000efeefd76c71c8660ca2
ampmmn <htmnymgw@gmail.com>
2025-09-21 10:23:00 +0900

    refactor : ColorCommand

--- src/soyokaze/commands/color/ColorCommand.cpp
+++ src/soyokaze/commands/color/ColorCommand.cpp
@@ -1,9 +1,9 @@
 #include "pch.h"
 #include "framework.h"
 #include "ColorCommand.h"
+#include "actions/clipboard/CopyClipboardAction.h"
 #include "commands/color/HSL.h"
 #include "icon/IconLoader.h"
-#include "commands/common/Clipboard.h"
 #include "SharedHwnd.h"
 #include "resource.h"
 #include <vector>
@@ -13,6 +13,7 @@
 #endif
 
 using namespace launcherapp::commands::common;
+using namespace launcherapp::actions::clipboard;
 
 namespace launcherapp {
 namespace commands {
@@ -158,13 +159,13 @@ CString ColorCommand::GetTypeDisplayName()
 	return TypeDisplayName();
 }
 
-BOOL ColorCommand::Execute(Parameter* param)
+bool ColorCommand::GetAction(uint32_t modifierFlags, Action** action)
 {
-	UNREFERENCED_PARAMETER(param);
-
+	UNREFERENCED_PARAMETER(modifierFlags);
 	// クリップボードにコピー
-	Clipboard::Copy(mName);
-	return TRUE;
+	*action = new CopyTextAction(mName);
+
+	return true;
 }
 
 
