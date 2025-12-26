752c91d17a690695a45df15942f6bd98dfcf14c0
ampmmn <htmnymgw@gmail.com>
2025-12-26 16:14:54 +0900

    feat : バージョン0.49.0

--- doc/manuals/source/conf.py
+++ doc/manuals/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-version = '0.48.1'
+version = '0.49.0'
 
 from configparser import ConfigParser
 config = ConfigParser()
