460939e7b1085ac91d8496a6ce32fc42267caf2a
ampmmn <htmnymgw@gmail.com>
2026-01-04 12:03:30 +0900

    docs : update conf.py

--- doc/manuals/source/conf.py
+++ doc/manuals/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-version = '0.49.0'
+version = '0.50.0'
 
 from configparser import ConfigParser
 config = ConfigParser()
