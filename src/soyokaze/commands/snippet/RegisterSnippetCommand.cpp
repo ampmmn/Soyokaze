bbfc32dfbe42411a7bd2b6a5aa9244e2c3109947
ampmmn <htmnymgw@gmail.com>
2026-08-14 13:45:07 +0900

    feat : newsnippetコマンドの廃止

--- src/soyokaze/commands/snippet/RegisterSnippetCommand.cpp
+++ src/soyokaze/commands/snippet/RegisterSnippetCommand.cpp
@@ -13,69 +13,69 @@
 #define new DEBUG_NEW
 #endif
 
-namespace launcherapp {
-namespace commands {
-namespace snippet {
-
-using ParameterBuilder = launcherapp::actions::core::ParameterBuilder;
-using CallbackAction = launcherapp::actions::builtin::CallbackAction;
-
-CString RegisterSnippetCommand::TYPE(_T("Builtin-RegisterSnippet"));
-CString RegisterSnippetCommand::DEFAULT_NAME(_T("newsnippet"));
-
-// BuiltinCommandFactory経由でインスタンスを生成できるようにするための手続き
-REGISTER_BUILTINCOMMAND(RegisterSnippetCommand)
-
-CString RegisterSnippetCommand::GetType()
-{
-	return TYPE;
-}
-
-RegisterSnippetCommand::RegisterSnippetCommand(LPCTSTR name) :
-	BuiltinCommandBase(name ? name : (LPCTSTR)DEFAULT_NAME)
-{
-	mDescription = _T("【クリップボードのテキストを定型文として登録】");
-	mCanSetConfirm = false;
-	mCanDisable = true;
-}
-
-RegisterSnippetCommand::~RegisterSnippetCommand()
-{
-}
-
-bool RegisterSnippetCommand::GetAction(const HOTKEY_ATTR& hotkeyAttr, Action** action)
-{
-	if (hotkeyAttr.GetModifiers() != 0) {
-		return false;
-	}
-
-	*action = new CallbackAction(_T("開く"), [&](Parameter*, String*) -> bool {
-		// ウインドウ経由でクリップボードのテキストを取得
-		CString clipboardText;
-		auto mainWnd = launcherapp::mainwindow::controller::MainWindowController::GetInstance();
-		mainWnd->GetClipboardString(clipboardText);
-	
-		RefPtr<ParameterBuilder> inParam(ParameterBuilder::Create(), false);
-		inParam->SetNamedParamString(_T("TEXT"), clipboardText);
-		SnippetCommand::NewDialog(inParam);
-	
-		// キャンセルされてもエラーダイアログを出さないようにするため、常にTRUEをかえす
-		return true;
-	});
-	return true;
-}
-
-HICON RegisterSnippetCommand::GetIcon()
-{
-	return IconLoader::Get()->LoadNewIcon();
-}
-
-launcherapp::core::Command* RegisterSnippetCommand::Clone()
-{
-	return new RegisterSnippetCommand();
-}
-
-} // end of namespace builtin
-} // end of namespace commands
-} // end of namespace launcherapp
+//namespace launcherapp {
+//namespace commands {
+//namespace snippet {
+//
+//using ParameterBuilder = launcherapp::actions::core::ParameterBuilder;
+//using CallbackAction = launcherapp::actions::builtin::CallbackAction;
+//
+//CString RegisterSnippetCommand::TYPE(_T("Builtin-RegisterSnippet"));
+//CString RegisterSnippetCommand::DEFAULT_NAME(_T("newsnippet"));
+//
+//// BuiltinCommandFactory経由でインスタンスを生成できるようにするための手続き
+//REGISTER_BUILTINCOMMAND(RegisterSnippetCommand)
+//
+//CString RegisterSnippetCommand::GetType()
+//{
+//	return TYPE;
+//}
+//
+//RegisterSnippetCommand::RegisterSnippetCommand(LPCTSTR name) :
+//	BuiltinCommandBase(name ? name : (LPCTSTR)DEFAULT_NAME)
+//{
+//	mDescription = _T("【クリップボードのテキストを定型文として登録】");
+//	mCanSetConfirm = false;
+//	mCanDisable = true;
+//}
+//
+//RegisterSnippetCommand::~RegisterSnippetCommand()
+//{
+//}
+//
+//bool RegisterSnippetCommand::GetAction(const HOTKEY_ATTR& hotkeyAttr, Action** action)
+//{
+//	if (hotkeyAttr.GetModifiers() != 0) {
+//		return false;
+//	}
+//
+//	*action = new CallbackAction(_T("開く"), [&](Parameter*, String*) -> bool {
+//		// ウインドウ経由でクリップボードのテキストを取得
+//		CString clipboardText;
+//		auto mainWnd = launcherapp::mainwindow::controller::MainWindowController::GetInstance();
+//		mainWnd->GetClipboardString(clipboardText);
+//	
+//		RefPtr<ParameterBuilder> inParam(ParameterBuilder::Create(), false);
+//		inParam->SetNamedParamString(_T("TEXT"), clipboardText);
+//		SnippetCommand::NewDialog(inParam);
+//	
+//		// キャンセルされてもエラーダイアログを出さないようにするため、常にTRUEをかえす
+//		return true;
+//	});
+//	return true;
+//}
+//
+//HICON RegisterSnippetCommand::GetIcon()
+//{
+//	return IconLoader::Get()->LoadNewIcon();
+//}
+//
+//launcherapp::core::Command* RegisterSnippetCommand::Clone()
+//{
+//	return new RegisterSnippetCommand();
+//}
+//
+//} // end of namespace builtin
+//} // end of namespace commands
+//} // end of namespace launcherapp
 
--- src/soyokaze/commands/snippet/RegisterSnippetCommand.h
+++ src/soyokaze/commands/snippet/RegisterSnippetCommand.h
@@ -1,31 +1,31 @@
 #pragma once
 
-#include "commands/builtin/BuiltinCommandBase.h"
-
-namespace launcherapp {
-namespace commands {
-namespace snippet {
-
-
-class RegisterSnippetCommand : public launcherapp::commands::builtin::BuiltinCommandBase
-{
-public:
-	RegisterSnippetCommand(LPCTSTR name = nullptr);
-	virtual ~RegisterSnippetCommand();
-
-	bool GetAction(const HOTKEY_ATTR& hotkeyAttr, Action** action) override;
-	HICON GetIcon() override;
-	launcherapp::core::Command* Clone() override;
-
-	CString GetType() override;
-	static CString TYPE;
-	static CString DEFAULT_NAME;
-
-	// BuiltinCommandFactory経由でインスタンスを生成できるようにするための手続き
-	DECLARE_BUILTINCOMMAND(RegisterSnippetCommand)
-};
-
-} // end of namespace builtin
-} // end of namespace commands
-} // end of namespace launcherapp
-
+//#include "commands/builtin/BuiltinCommandBase.h"
+//
+//namespace launcherapp {
+//namespace commands {
+//namespace snippet {
+//
+//
+//class RegisterSnippetCommand : public launcherapp::commands::builtin::BuiltinCommandBase
+//{
+//public:
+//	RegisterSnippetCommand(LPCTSTR name = nullptr);
+//	virtual ~RegisterSnippetCommand();
+//
+//	bool GetAction(const HOTKEY_ATTR& hotkeyAttr, Action** action) override;
+//	HICON GetIcon() override;
+//	launcherapp::core::Command* Clone() override;
+//
+//	CString GetType() override;
+//	static CString TYPE;
+//	static CString DEFAULT_NAME;
+//
+//	// BuiltinCommandFactory経由でインスタンスを生成できるようにするための手続き
+//	DECLARE_BUILTINCOMMAND(RegisterSnippetCommand)
+//};
+//
+//} // end of namespace builtin
+//} // end of namespace commands
+//} // end of namespace launcherapp
+//
